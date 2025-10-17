ced40f20e48dcfdf4e2eb5e57be274dd5c9485e4
Your Bebo <elbebanymostafa@gmail.com>
2025-10-17 19:21:33 +0300

    Initial project structure with .gitignore

---
+++ WMS_DEPI_GRAD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WMS_DEPI_GRAD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
